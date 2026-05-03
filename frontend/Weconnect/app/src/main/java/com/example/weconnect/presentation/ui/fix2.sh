0c1ceb57b902a34dffcd523481416306cf53d7ac
Do Dang Nguyen <112259140+LemonDangNguyen@users.noreply.github.com>
2026-05-03 09:28:43 +0700

    clear code

--- frontend/Weconnect/app/src/main/java/com/example/weconnect/presentation/ui/fix2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sed -i '' 's/binding.footerNavigationProfile.findViewById(R.id.btnHomeProfile);/binding.btnHomeProfile;/g' UserProfileActivity.java
-sed -i '' 's/binding.footerNavigationProfile.findViewById(R.id.btnMessagesProfile);/binding.btnMessagesProfile;/g' UserProfileActivity.java
-sed -i '' 's/binding.footerNavigationProfile.findViewById(R.id.btnNotificationsProfile);/binding.btnNotificationsProfile;/g' UserProfileActivity.java
